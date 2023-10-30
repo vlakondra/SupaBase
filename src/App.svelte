27f762db2bbe6c9a4a9b5fb979a7bd4334599170
vlakondra <kondrashkin.wladimir@yandex.ru>
2023-10-30 16:23:08 +0000

    add IF for data

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,37 @@
 <script>
     import { supabase } from "./store";
+	import { onMount } from "svelte";
 	export let name;
-console.log(supabase)
+	let data 
+
+	onMount(async () => {
+		let n = await supabase
+             .from('countries')
+             .select('id,name')
+			 console.log('N',n)
+        data=n.data
+	});
 </script>
 
 <main>
 	<h1>Hello {name}!!!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 <p>sbp_309267a93fc2ac91905a9a40479140d3569bddfa</p>
+
+{#if data}
+	 <!-- content here -->
+
+{#each data  as item}
+	 <div style='display:flex; flex-direction:row'>
+		<p>{item.id}</p>
+		<p>{item.name}</p>
+	 </div>
+{/each}
+{/if}
 </main>
 
 <style>
+	p{margin:10px}
 	main {
 		text-align: center;
 		padding: 1em;
